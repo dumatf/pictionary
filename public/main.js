7fc816304286566776428edabe2657cd13fd3ddb
dumatf <dumatf@users.noreply.github.com>
2015-05-26 11:06:27 -0400

    draw on mousedown only and broadcast to all clients

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 var pictionary = function() {
-	var canvas, context;
+	var canvas, context, drawing = 0;
+	var socket = io();
 
 //draw solid black circle with 6px radius
 	var draw = function(position) {
@@ -12,14 +13,28 @@ var pictionary = function() {
 	context = canvas[0].getContext('2d'); //create drawing context for canvas
 	canvas[0].width = canvas[0].offsetWidth;
 	canvas[0].height = canvas[0].offsetHeight;
-	canvas.on('mousemove', function(event) { //add 
-		var offset = canvas.offset();
-		var position = {x: event.pageX - offset.left,
-										y: event.pageY - offset.top};
-		draw(position);
+	canvas.on('mousemove', function(event) {
+		if (drawing) {
+			var offset = canvas.offset();
+			var position = {x: event.pageX - offset.left,
+											y: event.pageY - offset.top};
+			draw(position);
+			socket.emit('draw', position);
+		}
 	});
+
+	canvas.on('mousedown', function(event) {
+		drawing = 1;
+	});
+
+	canvas.on('mouseup', function(event) {
+		drawing = 0;
+	});
+
+	socket.on('draw', draw);
 };
 
 $(document).ready(function() {
+	//should the socket functionality be inside this block?
 	pictionary();
 });
