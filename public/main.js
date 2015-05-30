9d21bb8e8ba5f87b1c7bf386b9607b36e24d50a2
dumatf <dumatf@users.noreply.github.com>
2015-05-30 11:41:21 -0400

    handle no drawers/no guessers

--- public/main.js
+++ public/main.js
@@ -1,40 +1,81 @@
 var pictionary = function() {
-	var canvas, context, drawing = 0;
+	var canvas, context, drawing = 0, guessBox;
 	var socket = io();
 
-//draw solid black circle with 6px radius
+	// Draw black circle with 6px radius
 	var draw = function(position) {
 		context.beginPath();
 		context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
 		context.fill();
 	};
 
+	// Create click and drag drawing canvas
 	canvas = $('canvas');
-	context = canvas[0].getContext('2d'); //create drawing context for canvas
+	context = canvas[0].getContext('2d');
 	canvas[0].width = canvas[0].offsetWidth;
 	canvas[0].height = canvas[0].offsetHeight;
 	canvas.on('mousemove', function(event) {
 		if (drawing) {
 			var offset = canvas.offset();
 			var position = {x: event.pageX - offset.left,
-											y: event.pageY - offset.top};
+							y: event.pageY - offset.top};
 			draw(position);
 			socket.emit('draw', position);
 		}
 	});
+	canvas.on('mousedown', function(event) { drawing = 1; });
+	canvas.on('mouseup', function(event) { drawing = 0; });
 
-	canvas.on('mousedown', function(event) {
-		drawing = 1;
-	});
+	var clearCanvas = function() {
+		context.clearRect (0, 0, canvas[0].width, canvas[0].height);
+	};
 
-	canvas.on('mouseup', function(event) {
-		drawing = 0;
-	});
+	// Submit a guess 
+	var onKeyDown = function(event) {
+		if (event.keyCode != 13) { // Enter
+			return;
+		}
 
+		socket.emit('guess', guessBox.val());
+		guessBox.val('');
+	};
+
+	guessBox = $('#guess input');
+	guessBox.on('keydown', onKeyDown);
+
+	// Display guess to other clients
+	var addGuess = function(guess) {
+		$('#guesses').text(guess).show();
+		setTimeout(function() {
+			$('#guesses').fadeOut();
+		}, 2000);
+	};
+
+	// Set client as drawer or guesser
+	var setUser = function(word) {
+		if (word) {
+			$('#guess').hide();
+			$('#word').show();
+			$('#word b').text(word);	
+		} else {
+			$('#word').hide();
+			$('#guess').show();
+		}
+	};
+
+	// Display waiting message to drawer
+	var waitingForPlayers = function(message) {
+		$('#guesses').text(message).show();
+	}
+
+	// Receive events from server
 	socket.on('draw', draw);
+	socket.on('guess', addGuess);
+	socket.on('set user', setUser);
+	socket.on('reset', clearCanvas);
+	socket.on('wait', waitingForPlayers);
 };
 
 $(document).ready(function() {
-	//should the socket functionality be inside this block?
 	pictionary();
 });
--- server.js
+++ server.js
@@ -1,17 +1,73 @@
 var http = require('http');
 var express = require('express');
 var socket_io = require('socket.io');
+var WORDS = require('./words');
 
 var app = express();
 app.use(express.static('public'));
 
 var server = http.Server(app);
 var io = socket_io(server);
+var word, drawer, clients = [];
+
+var getRandomWord = function() {
+	return word = WORDS[Math.floor(Math.random() * WORDS.length)];
+}
 
 io.on('connection', function(socket) {
+	if (clients.length === 0) {
+		socket.emit('set user', getRandomWord());
+		socket.emit('wait', 'Waiting for guessers...');
+		drawer = socket;
+	} else if (clients.length === 1) {
+		// start the game
+		io.emit('reset');
+		io.emit('guess', 'Starting new game!');
+		socket.emit('set user');
+	} else {
+		socket.emit('set user');
+	}
+	clients.push(socket);
+
 	socket.on('draw', function(position) {
 		socket.broadcast.emit('draw', position);
+	});
+
+	socket.on('guess', function(guess) {
+		if (guess.toUpperCase() === word.toUpperCase()) {
+			socket.emit('guess', 'Correct!');
+			socket.emit('set user', getRandomWord());
+			drawer.emit('set user');
+			io.emit('reset');
+			drawer = this;
+		} else {
+			socket.emit('guess', 'Wrong!');
+			socket.broadcast.emit('guess', guess);	
+		}
+	});
+
+	socket.on('disconnect', function() {
+		var index = clients.indexOf(socket);
+		if (index != -1 ) {
+			clients.splice(index, 1);
+			console.log('removed client', clients.length);
+		}
+
+		// Randomly select new drawer
+		if (socket === drawer) {
+			drawer = clients[Math.floor(Math.random() * clients.length)];
+			drawer.emit('set user', getRandomWord());
+			io.emit('guess', 'Drawer left! Starting new game..');
+			io.emit('reset');
+		}
+
+		if (clients.length === 1) {
+			io.emit('wait', 'Waiting for guessers...');
+		}
 	});
 });
 
 server.listen(8080);
+
+// hide full ui panel until we know the client is the drawer
+// then render the drawer elements and the pictionary board
